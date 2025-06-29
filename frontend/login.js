040ea3df47b1955905c33d3e7d3a8045e3229b76
Manas Tripathy <manastripathy43@gmail.com>
2025-06-29 22:19:54 +0530

    login.js updated

--- frontend/login.js
+++ frontend/login.js
@@ -3,3 +3,22 @@ let captcha = document.querySelector(".form-captcha");
 let captchaCode = Math.floor(Math.random() * 90000) + 10000;
 console.log(captchaCode);
 captcha.innerText = captchaCode;
+
+// Username and Password fetching
+const signup = async () => {
+  const empId = document.getElementById("empId").value;
+  const password = document.getElementById("password").value;
+  const captcha = document.getElementById("captcha").value;
+
+  if (captcha == captchaCode) {
+    const res = await fetch("http://localhost:5000/etl/user", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ login: { empId, password } }),
+    });
+  }
+  const data = await res.json();
+  console.log(data);
+};
+
+document.getElementById("signup-btn").addEventListener("click", signup);
